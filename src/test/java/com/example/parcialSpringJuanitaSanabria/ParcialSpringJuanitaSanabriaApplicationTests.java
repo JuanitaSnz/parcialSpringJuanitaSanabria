e1039cc5827956299d33f8b67b8e1e8c40fc6fe4
Juanita Sanabria <93340989+JuanitaSnz@users.noreply.github.com>
2024-02-20 15:02:04 -0500

    Initial commit

---
+++ src/test/java/com/example/parcialSpringJuanitaSanabria/ParcialSpringJuanitaSanabriaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.parcialSpringJuanitaSanabria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ParcialSpringJuanitaSanabriaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
